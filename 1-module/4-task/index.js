685d3df30afca26710463fafca279b5c9883df4e
Varya63 <kvarvara63@mail.ru>
2023-10-05 00:21:06 +0400

    complete 4 task 1 module

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -1,3 +1,16 @@
 function checkSpam(str) {
-  // ваш код...
+  let newStr = str.toUpperCase();
+  // if (newStr.includes("XXX")){
+  //   return true;
+  // } else if (newStr.includes("1XBET")){
+  //   return true;
+  // } else {
+  //   return false;
+  // }
+
+  if (newStr.includes("XXX") || newStr.includes("1XBET")){
+    return true;
+  } else{
+    return false;
+  }
 }
