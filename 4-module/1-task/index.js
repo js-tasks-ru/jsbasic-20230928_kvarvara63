79e48e2664465e446f2e8d8ab5aa3125ddf3ad0b
Varya63 <kvarvara63@mail.ru>
2023-10-19 00:34:15 +0400

    complete 1 task 4 module

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,8 @@
 function makeFriendsList(friends) {
-  // ваш код...
-}
+    let ul = document.createElement('ul');
+    for (let key of friends){
+      let li = document.createElement('li');
+      ul.innerHTML = friends.map((key) => `<li>${key.firstName} ${key.lastName}</li>`);
+    }
+    return ul;
+  }
