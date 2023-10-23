29d227ecb785839fc17209d89a5dc00ff4f58e3c
Varya63 <kvarvara63@mail.ru>
2023-10-23 11:56:51 +0400

    difficulty with 3 task 4 module

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,26 @@
 function highlight(table) {
-  // ваш код...
+  let trs = table.querySelectorAll("tr");
+  for (let tr of trs){
+    if (tr.dataset.available === "true"){
+      tr.classList.add('available');
+    } else if (tr.dataset.available === "false"){
+      tr.classList.add('unavailable');
+    } else{
+      tr.hidden = true;
+    }
+  }
+
+    let gender = tr.cells[2];
+    if (gender.dataset.gender === "m"){
+      tr.classList.add("male");
+    } else {
+      tr.classList.add("female");
+    }
+
+    let age = tr.cells[1];
+    if (age < 18){
+      tr.style.textDecoration = "line-through";
+    } else {
+      return tr.style.textDecoration = "none";
+    }
 }
