b99f287604bd29b89be9c354ff1fb35d63a494bb
Varya63 <kvarvara63@mail.ru>
2023-10-29 14:07:27 +0400

    correct errors

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,24 +1,24 @@
 function highlight(table) {
   let trs = table.querySelectorAll("tr");
   for (let tr of trs){
-    if (tr.dataset.available === "true"){
+    if (tr.cells[3].dataset.available === "true"){
       tr.classList.add('available');
-    } else if (tr.dataset.available === "false"){
+    } else if (tr.cells[3].dataset.available === "false"){
       tr.classList.add('unavailable');
     } else{
       tr.hidden = true;
     }
   let gender = tr.cells[2];
-    if (gender.dataset.gender === "m"){
+    if (gender.textContent === "m"){
       tr.classList.add("male");
     } else {
       tr.classList.add("female");
     }
-    let age = tr.cells[1];
+    let age = tr.cells[1].textContent;
     if (age < 18){
       tr.style.textDecoration = "line-through";
     } else {
-      tr.style.textDecoration = "none"; // или лучше tr.style.textDecoration = ''; ?
+      tr.style.textDecoration = ""; 
     }
   }
 }
--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -2,19 +2,24 @@ function initCarousel() {
   let sliderInner = document.querySelector('.carousel__inner');
   let carouselSlide = document.querySelector('.carousel__slide');
 
-  let rightButton = document.querySelector('.carousel__arrow carousel__arrow_right');
-  let leftButton = document.querySelector('.carousel__arrow carousel__arrow_left');
+  let rightButton = document.querySelector('.carousel__arrow.carousel__arrow_right');
+  let leftButton = document.querySelector('.carousel__arrow.carousel__arrow_left');
   let numberSlider = 0;
+
+  removeButton(numberSlider);
 
   leftButton.addEventListener('click', event => {
       numberSlider -= 1;
-      sliderInner.style.transform = `translateX(-${numberSlider * 300}px)`;
+      sliderInner.style.transform = `translateX(-${numberSlider * sliderInner.offsetWidth}px)`;
+      removeButton(numberSlider);
   })
 
   rightButton.addEventListener('click', event => {
     numberSlider += 1;
-    sliderInner.style.transform = `translateX(${numberSlider * 300}px)`;
+    sliderInner.style.transform = `translateX(-${numberSlider * sliderInner.offsetWidth}px)`;
+    removeButton(numberSlider);
   })
+
   function removeButton(numberSlider){
     if (numberSlider == 3){
       rightButton.style.display = 'none';
@@ -25,6 +30,6 @@ function initCarousel() {
     } else if (numberSlider == 1 || numberSlider == 2 ){
       rightButton.style.display = '';
       leftButton.style.display = '';
-    } 
+    }
   }
 }
